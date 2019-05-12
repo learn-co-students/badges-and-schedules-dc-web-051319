6628a86dbc362bef250877759be47ccaaa7adfa3
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-12 00:23:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+require 'pry'
+
+
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = [] 
+  attendees.each do |name|
+  badges.push(badge_maker(name))
+end 
+badges
+end
+
+def assign_rooms(attendees)
+welcomes = []
+attendees.each.with_index(1) do |name, room|
+welcomes.push("Hello, #{name}! You'll be assigned to room #{room}!")
+end
+return welcomes
+end
+
+def printer(attendees)
+attendees.each do |n|
+  puts badge_maker(n) 
+end 
+assign_rooms(attendees).each do |welcome|
+  puts welcome 
+end 
+end 
+
+
+
+
+
+
